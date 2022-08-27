0d11233721b56658867fb525f81df786dfda955e
Adenir Cescon <adenircescon@outlook.com>
2022-08-26 22:18:38 -0300

    Alterações em funçoes

--- Aula Javascript/jQuery/App Dashboard/app.php
+++ Aula Javascript/jQuery/App Dashboard/app.php
@@ -1,31 +1,36 @@
 <?php
 
 //classe dashboard
-class Dashboard {
+class Dashboard
+{
 
 	public $data_inicio;
 	public $data_fim;
 	public $numeroVendas;
-	public $totalVendas; 
+	public $totalVendas;
 
-	public function __get($atributo) {
+	public function __get($atributo)
+	{
 		return $this->$atributo;
 	}
 
-	public function __set($atributo, $valor) {
+	public function __set($atributo, $valor)
+	{
 		$this->$atributo = $valor;
 		return $this;
 	}
 }
 
 //classe de conexão bd
-class Conexao {
+class Conexao
+{
 	private $host = 'localhost';
 	private $dbname = 'dashboard';
 	private $user = 'root';
 	private $pass = '';
 
-	public function conectar() {
+	public function conectar()
+	{
 		try {
 
 			$conexao = new PDO(
@@ -38,24 +43,26 @@ class Conexao {
 			$conexao->exec('set charset set utf8');
 
 			return $conexao;
-
 		} catch (PDOException $e) {
-			echo '<p>'.$e->getMessege().'</p>';
+			echo '<p>' . $e->getMessage() . '</p>';
 		}
 	}
 }
 
 //classe (model)
-class Bd {
+class Bd
+{
 	private $conexao;
 	private $dashboard;
 
-	public function __construct(Conexao $conexao, Dashboard $dashboard) {
+	public function __construct(Conexao $conexao, Dashboard $dashboard)
+	{
 		$this->conexao = $conexao->conectar();
 		$this->dashboard = $dashboard;
 	}
 
-	public function getNumeroVendas() {
+	public function getNumeroVendas()
+	{
 		$query = '
 			select 
 				count(*) as numero_vendas 
@@ -72,7 +79,8 @@ class Bd {
 		return $stmt->fetch(PDO::FETCH_OBJ)->numero_vendas;
 	}
 
-	public function getTotalVendas() {
+	public function getTotalVendas()
+	{
 		$query = '
 			select 
 				SUM(total) as total_vendas 
@@ -102,8 +110,8 @@ $mes = $competencia[1];
 
 $dias_do_mes = cal_days_in_month(CAL_GREGORIAN, $mes, $ano);
 
-$dashboard->__set('data_inicio', $ano.'-'.$mes.'-01');
-$dashboard->__set('data_fim', $ano.'-'.$mes.'-'.$dias_do_mes);
+$dashboard->__set('data_inicio', $ano . '-' . $mes . '-01');
+$dashboard->__set('data_fim', $ano . '-' . $mes . '-' . $dias_do_mes);
 
 
 $bd = new Bd($conexao, $dashboard);
@@ -111,7 +119,3 @@ $bd = new Bd($conexao, $dashboard);
 $dashboard->__set('numeroVendas', $bd->getNumeroVendas());
 $dashboard->__set('totalVendas', $bd->getTotalVendas());
 echo json_encode($dashboard);
-
-
-
-?>
